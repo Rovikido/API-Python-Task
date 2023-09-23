134855beced27c9cc318f25b2227846176ea0a3a
DragonSasha <51650290+DragonSasha@users.noreply.github.com>
2023-09-23 18:47:26 +0300

    updated models

--- menu/menu/models.py
+++ menu/menu/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from datetime import datetime
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
@@ -13,6 +15,7 @@ class Menu(models.Model):
     name = models.CharField(max_length=100)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     menu_data = models.JSONField()
+    menu_date = models.DateField(default=datetime.now().strftime('%Y-%m-%d'))
 
     def __str__(self):
         return self.name
@@ -21,10 +24,11 @@ class Menu(models.Model):
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    vote_date = models.DateField()
+    vote_date = models.DateField(default=datetime.now().strftime('%Y-%m-%d'))
 
     class Meta:
         unique_together = ('user', 'menu', 'vote_date')
 
     def __str__(self):
-        return f"Vote by {self.user.username} for {self.menu.name} on {self.vote_date}."
+        return f"Vote by {self.user.username} for {self.menu.name} \
+            on {self.vote_date}."
